33a28f7e8909c3bd9229a05bca399fbb5972515c
olamide-hash <olamidehash234@gmail.com>
2026-02-16 13:02:22 +0100

    Delete report.py

--- report.py
+++
@@ -1,9 +0,0 @@
-def generate_report(results):
-    print("\n===== SCAN REPORT =====")
-
-    for result in results:
-        print(f"\nURL: {result.get('url')}")
-        print(f"SQL Injection: {result.get('sql')}")
-        print(f"XSS: {result.get('xss')}")
-        headers = result.get('headers')
-        print(f"Missing Headers: {headers}")
